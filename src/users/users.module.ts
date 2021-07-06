b37c19c11561bae21645d69e3b88b0608fd47700
Ashley Porter <porter.ashley13@gmail.com>
2021-07-05 21:49:12 -0700

    remove duplicate data from users module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,17 +3,11 @@ import { User } from './user.entity';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Tether } from 'src/tethers/tether.entity';
-import { TethersController } from 'src/tethers/tethers.controller';
-import { TethersService } from 'src/tethers/tethers.service';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([User]),
-    TypeOrmModule.forFeature([Tether]),
-  ],
-  controllers: [UsersController, TethersController],
-  providers: [UsersService, TethersService],
-  exports: [UsersService, TethersService],
+  imports: [TypeOrmModule.forFeature([User])],
+  controllers: [UsersController],
+  providers: [UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
